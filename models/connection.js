3aaf294ae2a15ae54cb0dcd02a309f6f1bf4e7c5
idecay <greenwood.cm88@gmail.com>
2020-01-17 11:37:52 -0500

    configure server and connection files

--- models/connection.js
+++ models/connection.js
@@ -1,16 +1,16 @@
 /* Import the mongoose module
  *
  */
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 /* Step 1.
  *
- * TODO: replace <db-name> with the name of your mongo database. 
+ * TODO: replace <db-name> with the name of your mongo database.
  * This will need to change for every new project you create.
  *
  */
-const connectionString = process.env.MONGODB_URI || "mongodb://localhost/<db-name>";
-
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://localhost/ReadingList";
 
 /* Step 2
  *
@@ -18,11 +18,9 @@ const connectionString = process.env.MONGODB_URI || "mongodb://localhost/<db-nam
  *
  * NOTE: newUrlParser diables a deprecation warning
  */
-mongoose.connect(connectionString, { useNewUrlParser: true})
-  .then(() => {
-    console.log("connected to mongo at: " + connectionString);
-  });
-
+mongoose.connect(connectionString, { useNewUrlParser: true }).then(() => {
+  console.log("connected to mongo at: " + connectionString);
+});
 
 /* Export the mongoose object.
  *
@@ -30,4 +28,4 @@ mongoose.connect(connectionString, { useNewUrlParser: true})
  * across our different controllers.
  *
  */
-module.exports = mongoose
+module.exports = mongoose;
