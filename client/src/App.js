bc236748dd534991533f4d7d937d8df905e95140
idecay <greenwood.cm88@gmail.com>
2020-01-23 01:06:48 -0500

    configure routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HelloWorld from './components/HelloWorld.js'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./components/HomePage.js";
+import CreateBook from "./components/CreateBook.js";
+import MyLibrary from "./components/MyLibrary.js";
+import SingleBook from "./components/SingleBook";
+import "./App.css";
+import CreateGenre from "./components/CreateGenre";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <div className="navbar">
+          <nav>
+            <div className="logo">
+              <img
+                src="/booklogo.png"
+                alt="Logo"
+                width="100px"
+                height="100px"
+              />
+            </div>
+            <div className="links">
+              <Link to="/home">
+                <div className="link-item">Home</div>
+              </Link>
+              <Link to="/library">
+                <div className="link-item">My library</div>
+              </Link>
+              <Link to="/create">
+                <div className="link-item"> Create Book</div>
+              </Link>
+            </div>
+          </nav>
+        </div>
         <Switch>
-          <Route exact path="/" component={HelloWorld}/>
+          <Route exact path="/home" component={HomePage} />
+          <Route exact path="/library" component={MyLibrary} />
+          <Route exact path="/create" component={CreateBook} />
+          <Route path="/create/genre" component={CreateGenre} />
+          <Route path="/:bookId" component={SingleBook} />
         </Switch>
       </Router>
     </div>
