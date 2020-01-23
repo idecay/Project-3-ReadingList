d40de83d1ac69dd335672f79f75581b796e683ed
idecay <greenwood.cm88@gmail.com>
2020-01-23 01:08:56 -0500

    correct typo

--- controllers/book.js
+++ controllers/book.js
@@ -1,41 +1,9 @@
-/* Step 1 import express
- *
- */
 const express = require("express");
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the
- * controller you need.
- *
- */
 const bookApi = require("../models/book.js");
+const genreApi = require("../models/genre.js");
 
-/* Step 3
- *
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
 const bookRouter = express.Router();
-
-/* Step 4
- *
- * TODO: Put all request handlers here
- */
-
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */
 
 function getBookId(req, res) {
   const bookId = req.params.bookId;
@@ -54,7 +22,7 @@ function getBookAuthor(req, res) {
 
 bookRouter.get("/", async (req, res) => {
   try {
-    const books = await creatureApi.getAllBooks();
+    const books = await bookApi.getAllBooks();
     console.log(books);
     return res.json(books);
   } catch (e) {
